603629c4e9ea09a873118e8c3891b5b20f7475a5
Louiloui <162288706+Janinloui@users.noreply.github.com>
2025-03-07 18:20:32 +0100

    Intial Files - Hackathon

---
+++ src/enums/user.ts
@@ -0,0 +1,7 @@
+export enum UserCategory {
+  Ubermensch = 'Ubermensch',
+  Architect = 'Architect',
+  Engineer = 'Engineer',
+  Client = 'Client',
+  Contracter = 'Contracter',
+}
---
+++ src/state/tableStore.ts
@@ -0,0 +1,28 @@
+import { create } from 'zustand';
+import { SlabType } from '../types/slabType';
+import { UserCategory } from '../enums/user';
+import { StartData } from './data';
+import { DefaultRenderValues } from '../table/attributeDefinition';
+
+type TableStore = {
+  elements: SlabType[];
+  updateElement: (element: SlabType) => void;
+  userCategory: UserCategory;
+  setUserCategory: (c: UserCategory) => void;
+  userAttributeMap: Record<UserCategory, string[]>;
+  setUserAttributeMap: (userCategory: UserCategory, attributes: string[]) => void;
+};
+
+export const useTableStore = create<TableStore>((set) => ({
+  elements: StartData,
+  updateElement: (element: SlabType) =>
+    set((s) => {
+      const index = s.elements.findIndex((e) => e.id === element.id);
+      if (index !== -1) s.elements[index] = { ...element };
+      return { elements: [...s.elements] };
+    }),
+  userCategory: UserCategory.Ubermensch,
+  setUserCategory: (userCategory: UserCategory) => set((s) => ({ userCategory })),
+  userAttributeMap: DefaultRenderValues,
+  setUserAttributeMap: (userCategory, attributes) => set((s) => ({ userAttributeMap: { ...s.userAttributeMap, [userCategory]: attributes } })),
+}));
