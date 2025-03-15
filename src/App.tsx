1e5350cc566b2b3df5bc3e471e415c6eda49b0b1
JonasWard <43600893+JonasWard@users.noreply.github.com>
2025-03-15 16:34:17 +0100

    feat: added three scene closes #5

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import { SlabTable } from './table/SlabTable';
+import { ThreeScene } from './webgl/ThreeScene';
 
 export const App: React.FC = () => {
-  return <SlabTable />;
+  return (
+    <>
+      <ThreeScene />
+      <SlabTable />
+    </>
+  );
 };
