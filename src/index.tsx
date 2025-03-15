3a979def889daca7ddb71cd7127714c3ca7ea854
JonasWard <43600893+JonasWard@users.noreply.github.com>
2025-03-15 21:47:18 +0100

    feat: added header when relevant closes #11

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import { Viewer } from './Viewer';
@@ -7,6 +7,14 @@ import { Slab2ReuseRoutes } from './enums/routes';
 import { SlabTable } from './table/SlabTable';
 import { ExampleData } from './tests/ExampleData';
 import { Landing } from './Landing';
+import { NavigationHeader } from './NavigationHeader';
+
+const wrapper = (node: ReactNode) => (
+  <>
+    <NavigationHeader />
+    {node}
+  </>
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
@@ -14,10 +22,17 @@ root.render(
     <Router>
       <Routes>
         <Route element={<Landing />} path='/' />
-        <Route element={<ExampleData />} path={Slab2ReuseRoutes.ExampleData} />
-        <Route element={<Viewer />} path={Slab2ReuseRoutes.Viewer} />
-        <Route element={<ThreeScene />} path={Slab2ReuseRoutes.ThreeOnly} />
-        <Route element={<SlabTable />} path={Slab2ReuseRoutes.TableOnly} />
+        <Route element={wrapper(<ExampleData />)} path={Slab2ReuseRoutes.ExampleData} />
+        <Route element={wrapper(<Viewer />)} path={Slab2ReuseRoutes.Viewer} />
+        <Route
+          element={wrapper(
+            <div style={{ height: '100svh' }}>
+              <ThreeScene />
+            </div>
+          )}
+          path={Slab2ReuseRoutes.ThreeOnly}
+        />
+        <Route element={wrapper(<SlabTable />)} path={Slab2ReuseRoutes.TableOnly} />
       </Routes>
     </Router>
   </React.StrictMode>
