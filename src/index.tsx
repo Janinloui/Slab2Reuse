7ec179e2e04e1e7abc59c709eaab604487ff267e
JonasWard <43600893+JonasWard@users.noreply.github.com>
2025-03-15 20:44:27 +0100

    feat: added routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
-import { App } from './App';
+import { Viewer } from './Viewer';
+import { ThreeScene } from './webgl/ThreeScene';
+import { Slab2ReuseRoutes } from './enums/routes';
+import { SlabTable } from './table/SlabTable';
+import { ExampleData } from './tests/ExampleData';
+import { Landing } from './Landing';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route element={<App />} path='/:stateString' />
-        <Route element={<App />} path='/' />
+        <Route element={<Landing />} path='/' />
+        <Route element={<ExampleData />} path={Slab2ReuseRoutes.ExampleData} />
+        <Route element={<Viewer />} path={Slab2ReuseRoutes.Viewer} />
+        <Route element={<ThreeScene />} path={Slab2ReuseRoutes.ThreeOnly} />
+        <Route element={<SlabTable />} path={Slab2ReuseRoutes.TableOnly} />
       </Routes>
     </Router>
   </React.StrictMode>
