d1c2b9a5d21658775bc7aea4075a322b0e466e24
JonasWard <43600893+JonasWard@users.noreply.github.com>
2025-03-15 19:34:16 +0100

    feat: created attribute names enum

---
+++ src/enums/attributeNames.ts
@@ -0,0 +1,17 @@
+export enum SlabKeyType {
+  Id = 'id',
+  PlanReference = 'planReference',
+  Location_x = 'location_x',
+  Location_y = 'location_y',
+  Location_z = 'location_z',
+  Strength = 'strength',
+  Dimensions_l = 'dimensions_l',
+  Dimensions_w = 'dimensions_w',
+  Dimensions_h = 'dimensions_h',
+  Liveload = 'liveload',
+  TypeOfElement = 'typeOfElement',
+  RebarDiameterTop = 'rebarDiameterTop',
+  RebarAmountTop = 'rebarAmountTop',
+  RebarDiameterBottom = 'rebarDiameterBottom',
+  RebarAmountBottom = 'rebarAmountBottom',
+}
