8f387241d686418f05aab9a31b2acc1a79eb006c
JonasWard <43600893+JonasWard@users.noreply.github.com>
2025-03-15 19:37:18 +0100

    feat: SlabKeyType record for type casting

--- src/types/slabType.ts
+++ src/types/slabType.ts
@@ -17,3 +17,21 @@ export type SlabType = {
   [SlabKeyType.RebarDiameterBottom]: number;
   [SlabKeyType.RebarAmountBottom]: number;
 };
+
+export const SlabTypeValueMap: Record<SlabKeyType, 'number' | 'string'> = {
+  [SlabKeyType.Id]: 'string',
+  [SlabKeyType.PlanReference]: 'string',
+  [SlabKeyType.Location_x]: 'number',
+  [SlabKeyType.Location_y]: 'number',
+  [SlabKeyType.Location_z]: 'number',
+  [SlabKeyType.Strength]: 'string',
+  [SlabKeyType.Dimensions_l]: 'number',
+  [SlabKeyType.Dimensions_w]: 'number',
+  [SlabKeyType.Dimensions_h]: 'number',
+  [SlabKeyType.Liveload]: 'number',
+  [SlabKeyType.TypeOfElement]: 'string',
+  [SlabKeyType.RebarDiameterTop]: 'number',
+  [SlabKeyType.RebarAmountTop]: 'number',
+  [SlabKeyType.RebarDiameterBottom]: 'number',
+  [SlabKeyType.RebarAmountBottom]: 'number',
+};
