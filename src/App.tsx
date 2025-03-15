c2e671d4ffe7fa64156e17068c8ac568887b7638
JonasWard <43600893+JonasWard@users.noreply.github.com>
2025-03-15 19:42:23 +0100

    feat: add boiler plate upload to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
+import { CsvUpload } from './table/io/CsvUpload';
 import { SlabTable } from './table/SlabTable';
 import { ThreeScene } from './webgl/ThreeScene';
 
 export const App: React.FC = () => {
   return (
     <>
-      <ThreeScene />
-      <SlabTable />
+      <ThreeScene key='scene' />
+      <SlabTable key='slabTable' />
+      <CsvUpload key='csvUpload' />
     </>
   );
 };
