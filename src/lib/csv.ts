4fc14e30e019f3f58bfc61f1ae98fdd37857c2d8
JonasWard <43600893+JonasWard@users.noreply.github.com>
2025-03-15 19:56:59 +0100

    doc: added descriptions to csv methods

--- src/lib/csv.ts
+++ src/lib/csv.ts
@@ -2,8 +2,16 @@ import { SlabKeyType } from '../enums/attributeNames';
 import { SlabType, SlabTypeValueMap } from '../types/slabType';
 
 export type CsvData = Record<string, string>[];
+/**
+ * Internal helper type for mapping a CSV to a Partial<SlabType> Array
+ */
 type MappingTable = Partial<Record<string, SlabKeyType>>;
 
+/**
+ * Method than constructs the mapping table by trying to match the attribute names in the CSV to thouse defined in the SlabTypeKey enum
+ * @param csvAttributes
+ * @returns MappingTable
+ */
 const getFuzzilyMatchedAttributeTable = (csvAttributes: string[]): MappingTable => {
   const slabKeys = Object.values(SlabKeyType);
 
@@ -18,11 +26,23 @@ const getFuzzilyMatchedAttributeTable = (csvAttributes: string[]): MappingTable 
   );
 };
 
-const canMapAttributes = (mappingTable: Partial<Record<string, SlabKeyType>>, requiredKeys: SlabKeyType[]): boolean => {
+/**
+ * Method that verifies whether it makes sense to use the CSV by checking whether defined SlabKeyType keys have been set or not
+ * @param mappingTable - MappingTable
+ * @param requiredKeys - SlabKeyType[]
+ * @returns boolean
+ */
+const canMapAttributes = (mappingTable: MappingTable, requiredKeys: SlabKeyType[]): boolean => {
   const requiredKeysSet = new Set(requiredKeys);
   return requiredKeysSet.intersection(new Set(Object.values(mappingTable))).size === requiredKeysSet.size;
 };
 
+/**
+ * Method that tries to parse the CSV Entry (key, value) to a Partial<SlabType>: Record<SlabKeyType, number | string>
+ * @param entry - Record<string, string>
+ * @param mappingTable
+ * @returns Record<SlabKeyType, number | string> matching Partial<SlabType>
+ */
 const mapEntry = (entry: Record<string, string>, mappingTable: MappingTable): Partial<SlabType> =>
   Object.fromEntries(
     Object.entries(entry)
@@ -31,7 +51,14 @@ const mapEntry = (entry: Record<string, string>, mappingTable: MappingTable): Pa
   );
 const getMappedData = (csvData: CsvData, mappingTable: MappingTable): Partial<SlabType>[] => csvData.map((entry) => mapEntry(entry, mappingTable));
 
-const getSlabTypeFromCsv = (csvData: CsvData, requiredKeys: SlabKeyType[] = [SlabKeyType.Id, SlabKeyType.PlanReference]): Partial<SlabType>[] => {
+/**
+ * Method that tries to parse the CSVData to a Partial<SlabType> Array
+ * Will throw an error if some data is not set
+ * @param csvData - Record<string, string>[];
+ * @param requiredKeys - SlabKeyType[]
+ * @returns Partial<SlabType>[]
+ */
+const getSlabTypesFromCsv = (csvData: CsvData, requiredKeys: SlabKeyType[] = [SlabKeyType.Id, SlabKeyType.PlanReference]): Partial<SlabType>[] => {
   const csvAttributes = new Set(csvData.map((entry) => Object.keys(entry)).flat());
   const mappingTable = getFuzzilyMatchedAttributeTable([...csvAttributes]);
 
@@ -39,10 +66,31 @@ const getSlabTypeFromCsv = (csvData: CsvData, requiredKeys: SlabKeyType[] = [Sla
   return getMappedData(csvData, mappingTable);
 };
 
+/**
+ * Wrapper method that adds an id to a Partial<SlabType> Array (init from CSV)
+ * @param data - Partial<SlabType>[]
+ * @returns Partial<SlabType>[] with an id set
+ */
 const addNewIds = (data: Partial<SlabType>[]): Partial<SlabType>[] => data.map((entry, i) => ({ ...entry, [SlabKeyType.Id]: i.toString() }));
 const hasAttribute = (data: Partial<SlabType>[], attribute: SlabKeyType): boolean => data.every((e) => e[attribute] !== undefined);
 
+/**
+ * Don't use with existing projects
+ * Method that returns a Partial<SlabType> Array from csv data, populate with NEW IDS
+ * @param csvData - CsvData
+ * @returns Partial<SlabType>[] with an id set
+ */
 export const initNewProject = (csvData: CsvData): Partial<SlabType>[] => {
-  const slabData = getSlabTypeFromCsv(csvData, [SlabKeyType.PlanReference]);
+  const slabData = getSlabTypesFromCsv(csvData, [SlabKeyType.PlanReference]);
+  return hasAttribute(slabData, SlabKeyType.Id) ? slabData : addNewIds(slabData);
+};
+
+/**
+ * Method that returns a Partial<SlabType> Array from csv data
+ * @param csvData - CsvData
+ * @returns Partial<SlabType>[]
+ */
+export const loadProject = (csvData: CsvData): Partial<SlabType>[] => {
+  const slabData = getSlabTypesFromCsv(csvData, [SlabKeyType.PlanReference, SlabKeyType.Id]);
   return hasAttribute(slabData, SlabKeyType.Id) ? slabData : addNewIds(slabData);
 };
