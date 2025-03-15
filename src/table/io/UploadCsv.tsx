9f7820b6a6c667297d5d6de662804f33d6deee5f
JonasWard <43600893+JonasWard@users.noreply.github.com>
2025-03-15 20:43:24 +0100

    feat: new Antd Upload

---
+++ src/table/io/UploadCsv.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { UploadOutlined } from '@ant-design/icons';
+import { Button, message, Upload } from 'antd';
+import Papa from 'papaparse';
+import { CsvData, initNewProject, loadProject } from '../../lib/csv';
+import { useTableStore } from '../../state/tableStore';
+import { useNavigate } from 'react-router-dom';
+import { Slab2ReuseRoutes } from '../../enums/routes';
+
+const papaConfig: Papa.ParseConfig<unknown, undefined> & {
+  download?: false | undefined;
+  worker?: false | undefined;
+} = {
+  header: true,
+};
+
+type UploadCSVProps = {
+  initNew?: boolean;
+};
+
+export const UploadCSV: React.FC<UploadCSVProps> = ({ initNew }) => {
+  const navigate = useNavigate();
+
+  return (
+    <Upload
+      accept='.csv'
+      showUploadList={false}
+      beforeUpload={(file) => {
+        const reader = new FileReader();
+
+        reader.onload = (e) => {
+          const result = Papa.parse(e.target!.result as any, papaConfig);
+          try {
+            const elements = initNew ? initNewProject(result.data as CsvData) : loadProject(result.data as CsvData);
+            useTableStore.setState((s) => ({ elements }));
+          } catch (e) {
+            message.error('importing the csv failed, check the console for more information');
+            console.error(e);
+          }
+          navigate(Slab2ReuseRoutes.Viewer);
+        };
+        reader.readAsText(file);
+
+        // Prevent upload
+        return false;
+      }}
+    >
+      <Button icon={<UploadOutlined />}>{initNew ? 'start a new project from a csv' : 'load in an existing project'} </Button>
+    </Upload>
+  );
+};
