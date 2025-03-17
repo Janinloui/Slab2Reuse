cb1188fb21deb1f15a45fca7dd91f029a97892fe
JonasWard <43600893+JonasWard@users.noreply.github.com>
2025-03-17 14:09:38 +0100

    feat: add element component

---
+++ src/element/AddElement.tsx
@@ -0,0 +1,55 @@
+import { Button, Drawer, Form, Input } from 'antd';
+import { SlabType } from '../types/slabType';
+import { useRef, useState } from 'react';
+import { DefaultRenderValues, RenderLocal, suffixMap, typeRenderer } from '../table/attributeDefinition';
+import { useTableStore } from '../state/tableStore';
+import { SlabKeyType } from '../enums/attributeNames';
+import { BiPlus } from 'react-icons/bi';
+
+export const AddElement: React.FC = () => {
+  const [element] = useState<Partial<SlabType>>({});
+  const activeGlobalUserCategory = useTableStore((s) => s.userCategory);
+  const [open, setOpen] = useState(false);
+  const formRef = useRef<any>(null);
+
+  function addElement() {
+    if (!formRef.current) return;
+    const values = formRef.current.getFieldsValue();
+    // casting the values that are in the suffix map to numbers
+    Object.keys(suffixMap).forEach((k) => (values[k] = Number(values[k])));
+    useTableStore.getState().addElement(values as Partial<SlabType>);
+    setOpen(false);
+  }
+
+  return (
+    <>
+      <Button onClick={() => setOpen(true)}>
+        <BiPlus /> add entry
+      </Button>
+      <Drawer
+        onClose={() => setOpen(false)}
+        open={open}
+        footer={
+          <span className='flex gap-4 py-2'>
+            <Button onClick={addElement} type='primary'>
+              add element
+            </Button>
+            <Button onClick={() => setOpen(false)}>cancel</Button>
+          </span>
+        }
+      >
+        {open && element ? (
+          <Form<Partial<SlabType>> ref={formRef} initialValues={element} title={typeRenderer(element)} layout='vertical' autoComplete='off'>
+            {Object.values(SlabKeyType)
+              .filter((k) => k !== SlabKeyType.Id)
+              .map((v) => (
+                <Form.Item label={RenderLocal[v as SlabKeyType]} name={v}>
+                  <Input />
+                </Form.Item>
+              ))}
+          </Form>
+        ) : null}
+      </Drawer>
+    </>
+  );
+};
