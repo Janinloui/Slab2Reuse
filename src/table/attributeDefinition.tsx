0ab110e766fe9e9b6652eae7fc4e63c448b5f59f
JonasWard <43600893+JonasWard@users.noreply.github.com>
2025-03-20 20:25:19 +0100

    chore: clean-up attribute table

--- src/table/attributeDefinition.tsx
+++ src/table/attributeDefinition.tsx
@@ -7,16 +7,16 @@ const EditKey = 'edit';
 
 //maps atrributes to their respective suffixes (Units)
 export const suffixMap: Partial<Record<SlabKeyType | DerivativeAttributeNames, string>> = {
-  location_x: 'm',
-  location_y: 'm',
-  location_z: 'm',
-  weight: 'kg',
-  dimensions_l: 'mm',
-  dimensions_w: 'mm',
-  dimensions_h: 'mm',
-  liveload: 'kN/m2',
-  rebarDiameterTop: 'mm',
-  rebarDiameterBottom: 'mm',
+  [SlabKeyType.Location_x]: 'm',
+  [SlabKeyType.Location_y]: 'm',
+  [SlabKeyType.Location_z]: 'm',
+  [DerivativeAttributeNames.Weight]: 'kg',
+  [SlabKeyType.Dimensions_l]: 'mm',
+  [SlabKeyType.Dimensions_w]: 'mm',
+  [SlabKeyType.Dimensions_h]: 'mm',
+  [SlabKeyType.Liveload]: 'kN/m2',
+  [SlabKeyType.RebarDiameterTop]: 'mm',
+  [SlabKeyType.RebarDiameterBottom]: 'mm',
   [SlabKeyType.Yaw]: '°',
 };
 
