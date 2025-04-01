6bdf554e9c18d5b5df5b1afffee307f3bc83eff5
Louiloui <162288706+Janinloui@users.noreply.github.com>
2025-04-01 21:46:14 +0200

    Fixes #45 define BuildingType

--- src/types/buildingType.ts
+++ src/types/buildingType.ts
@@ -1,23 +1,38 @@
-import { LocationType } from './locationType';
 import { ValueType } from './valueType';
+import { LocationType } from './locationType';
+import { UserType } from './userType';  
 
 enum BuildingTypeKeys {
   Id = 'buildingId',
   Location = 'buildingLocation',
-  OwnerId = 'buildingOwnerId',
   Address = 'buildingAddress',
+  User = 'buildingOwner',
+  FormerUse = 'buildingFormerUse',
+  GFA = 'buildingGFA',
+  Complexity = 'buildingComplexity',
+  Img = 'buildingImg'
 }
 
 export type BuildingType = {
   [BuildingTypeKeys.Id]: string;
   [BuildingTypeKeys.Location]: LocationType;
   [BuildingTypeKeys.Address]: string;
-  [BuildingTypeKeys.OwnerId]: string;
+  [BuildingTypeKeys.User]: UserType;
+  [BuildingTypeKeys.FormerUse]: string;
+  [BuildingTypeKeys.GFA]: number;
+  [BuildingTypeKeys.Complexity]: number;
+  [BuildingTypeKeys.Img]: string;
+  locationType: LocationType;
 };
 
 export const BuildingTypeValueMap: Record<BuildingTypeKeys, ValueType> = {
   [BuildingTypeKeys.Id]: 'string',
   [BuildingTypeKeys.Location]: 'LocationType',
   [BuildingTypeKeys.Address]: 'string',
-  [BuildingTypeKeys.OwnerId]: 'string',
+  [BuildingTypeKeys.User]: 'UserType',
+  [BuildingTypeKeys.FormerUse]: 'string',
+  [BuildingTypeKeys.GFA]: 'number',
+  [BuildingTypeKeys.Complexity]: 'number',
+  [BuildingTypeKeys.Img]: 'string',
 };
+
