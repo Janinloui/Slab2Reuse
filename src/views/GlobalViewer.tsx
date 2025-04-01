dac7536fe7251e87d09af67872b7ff22cc00017f
JonasWard <43600893+JonasWard@users.noreply.github.com>
2025-04-01 17:11:05 +0200

    feat: GlobalViewer component

---
+++ src/views/GlobalViewer.tsx
@@ -0,0 +1,4 @@
+import { Viewer } from '../enums/viewer';
+import { ViewState } from '../enums/viewState';
+
+export const GlobalViewer: React.FC<{ viewer: Viewer; viewState: ViewState }> = ({ viewer, viewState }) => {};
