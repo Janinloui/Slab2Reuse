86e34efe19e5072947e68d933ed308e93165eb53
JonasWard <43600893+JonasWard@users.noreply.github.com>
2025-04-01 09:36:49 +0200

    feat: defined Viewer enum

---
+++ src/enums/viewer.ts
@@ -0,0 +1,17 @@
+/**
+ * Enum which shows all the views for which unqiue key combinations should be displayed
+ */
+export enum Viewer {
+  ArchiveProjectLevel = 'archive-project-level',
+  ArchiveElementLevelGeometry = 'archive-element-geometry',
+  ArchiveElementMaterial = 'archive-element-material',
+  ArchiveElementCapacity = 'archive-element-capacity',
+  ArchiveReusePotential = 'archive-reuse-potential',
+  OnSiteTransport = 'on-site-transport',
+  OnSiteVisualInspection = 'on-site-visual-inspection',
+  OnSiteReboundTesting = 'on-site-rebound-testing',
+  OnSiteReinforcementScreening = 'on-site-reinforcement-screening',
+  OnSiteDeconstruction = 'on-site-deconstruction',
+  LabCoreTesting = 'lab-core-testing',
+  LabFullScaleTest = 'lab-full-scale-test',
+}
