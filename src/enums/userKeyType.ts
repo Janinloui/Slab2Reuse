18aef720c2ad49cb919d957d1d191673be2d6fe2
Louiloui <162288706+Janinloui@users.noreply.github.com>
2025-04-02 10:11:49 +0200

    moved enum

---
+++ src/enums/userKeyType.ts
@@ -0,0 +1,13 @@
+export enum UserKeyType {
+    Id = 'userId',
+    Name = 'userName',
+    Address = 'userAddress',
+    Company = 'userCompany',
+    Mail = 'userMail'
+  }
+
+export enum UserCategory {
+    Institution = 'Institution',
+    Individual = 'Individual',
+    Owner = 'Owner'
+}
