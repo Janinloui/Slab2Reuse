f127982ef2cb3eb431a5538d400d9350fd427ad7
Louiloui <162288706+Janinloui@users.noreply.github.com>
2025-04-02 10:16:05 +0200

    Added valueTypes

--- src/types/valueType.ts
+++ src/types/valueType.ts
@@ -1,1 +1,1 @@
-export type ValueType = 'number' | 'string' | 'LocationType' | 'UserType' | 'enum' | 'numberArray' | 'nestedNumberArray' | 'stringPairArray' | 'optional';
+export type ValueType = 'number' | 'string' | 'LocationType' | 'UserType' | 'enum' | 'numberArray' | 'nestedNumberArray' | 'stringPairArray' | 'optional'|'UserCategory'|'RebarConfiguration'|'CrossSectionCategory';
