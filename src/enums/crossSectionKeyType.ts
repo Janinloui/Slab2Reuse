5707ca31e0ea3705b1231326adc61bc8973cfc05
Louiloui <162288706+Janinloui@users.noreply.github.com>
2025-04-02 10:11:34 +0200

    moved enum

---
+++ src/enums/crossSectionKeyType.ts
@@ -0,0 +1,9 @@
+
+export enum CrossSectionKeyType {
+    id = 'crossSectionId',
+    width = 'crossSectionWidth',
+    height = 'crossSectionHeight',
+    moment = 'crossSectionMoment',
+    shear = 'crossSectionShear',
+    normal = 'crossSectionNormal',
+    }
