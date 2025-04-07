6a789171e0e34f8591da2689bd0e761459f34527
Jonas Ward <jonas.vandenbulcke@gmail.com>
2025-04-07 20:19:27 +0200

    feat: added missing data for the destructive test

--- src/types/destructiveTestType.ts
+++ src/types/destructiveTestType.ts
@@ -3,14 +3,32 @@ import { ValueType } from './valueType';
 
 export type DestructiveTestType = {
   [DestructiveTestKeyType.GeometryTypeId]: string;
+  [DestructiveTestKeyType.Date]: string;
   [DestructiveTestKeyType.ImgLongitudinal]: string;
   [DestructiveTestKeyType.ImgTransverse]: string;
   [DestructiveTestKeyType.UserId]: string;
+  [DestructiveTestKeyType.ShearStrength]: number;
+  [DestructiveTestKeyType.CompressiveStrength]: number;
+  [DestructiveTestKeyType.TensileStrength]: number;
+  [DestructiveTestKeyType.YoungsModulus]: number;
+  [DestructiveTestKeyType.MomentCapacity]: number;
+  [DestructiveTestKeyType.ShearCapacity]: number;
+  [DestructiveTestKeyType.NormalCapacity]: number;
+  [DestructiveTestKeyType.Density]: number;
 };
 
 export const DestructiveTestValueMap: Record<DestructiveTestKeyType, ValueType> = {
   [DestructiveTestKeyType.GeometryTypeId]: 'string',
   [DestructiveTestKeyType.ImgLongitudinal]: 'string',
   [DestructiveTestKeyType.ImgTransverse]: 'string',
-  [DestructiveTestKeyType.UserId]: 'string'
+  [DestructiveTestKeyType.UserId]: 'string',
+  [DestructiveTestKeyType.Date]: 'string',
+  [DestructiveTestKeyType.ShearStrength]: 'number',
+  [DestructiveTestKeyType.CompressiveStrength]: 'number',
+  [DestructiveTestKeyType.TensileStrength]: 'number',
+  [DestructiveTestKeyType.YoungsModulus]: 'number',
+  [DestructiveTestKeyType.MomentCapacity]: 'number',
+  [DestructiveTestKeyType.ShearCapacity]: 'number',
+  [DestructiveTestKeyType.NormalCapacity]: 'number',
+  [DestructiveTestKeyType.Density]: 'number'
 };
