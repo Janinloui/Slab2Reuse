e1233f22617f82bdeb5aab5884b1dfa46283c532
Jonas Ward <jonas.vandenbulcke@gmail.com>
2025-04-10 11:14:04 +0200

    feat: ObjectRender & IdReferenceRenderer

--- src/generic/GenericUIRenderer.tsx
+++ src/generic/GenericUIRenderer.tsx
@@ -1,4 +1,4 @@
-import { Input, InputNumber, Select } from 'antd';
+import { Button, Input, InputNumber, Popover, Select } from 'antd';
 import { ComponentCategory } from '../enums/componentCategory';
 import { CrossSectionCategory } from '../enums/crossSectionCategory';
 import { MaterialCategory } from '../enums/materialCategory';
@@ -6,8 +6,26 @@ import { RebarCategory } from '../enums/rebarCategory';
 import { UserCategory } from '../enums/user';
 import { ValueType } from '../types/valueType';
 import { AllKeyMap } from '../types/allKeyMap';
-import { ReactNode } from 'react';
+import { ReactNode, useState } from 'react';
 import { VisualCondition } from '../enums/visualCondition';
+import { IdKeys, IdKeysCollectionMap, IdKeysType } from '../types/idKeyMap';
+import { useCollectionStore } from '../state/collectionStore';
+import { CollectionName } from '../enums/collectionName';
+import { BuildingType } from '../types/buildingType';
+import { BuildingKeyType } from '../enums/buildingKeyType';
+import { RebarType } from '../types/rebarType';
+import { RebarKeyType } from '../enums/rebarKeyType';
+import { MaterialType } from '../types/materialType';
+import { MaterialKeyType } from '../enums/materialKeyType';
+import { CrossSectionType } from '../types/crossSectionType';
+import { CrossSectionKeyType } from '../enums/crossSectionKeyType';
+import { GeometryType } from '../types/geometryType';
+import { GeometryKeyType } from '../enums/geometryKeyType';
+import { ComponentKeyType } from '../enums/componentKeyType';
+import { ComponentType } from '../types/componentType';
+import { UserType } from '../types/userType';
+import { UserKeyType } from '../enums/userKeyType';
+import { MissingData } from '../table/MissingData';
 
 //number
 const NumberRenderer = (v: number) => <InputNumber value={v} />;
@@ -91,13 +109,55 @@ const ArrayRenderer: React.FC<{ items: any[]; valueType: ValueType }> = ({ items
   </div>
 );
 
+const ObjectRender: React.FC<{ name: string; value: any }> = ({ name: k, value }) => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <Popover
+      open={open}
+      content={
+        <>
+          <GenericUIRenderer item={value} />
+          <Button type='primary' onClick={() => setOpen(false)}>
+            close
+          </Button>
+        </>
+      }
+      title={k}
+    >
+      <Button onClick={() => setOpen(true)}>{k}</Button>
+    </Popover>
+  );
+};
+
+const findMethodMap = {
+  [CollectionName.Buildings]: (e: BuildingType, id: string) => e[BuildingKeyType.Id] === id,
+  [CollectionName.Users]: (e: UserType, id: string) => e[UserKeyType.Id] === id,
+  [CollectionName.Components]: (e: ComponentType, id: string) => e[ComponentKeyType.Id] === id,
+  [CollectionName.Geometries]: (e: GeometryType, id: string) => e[GeometryKeyType.Id] === id,
+  [CollectionName.CrossSections]: (e: CrossSectionType, id: string) => e[CrossSectionKeyType.Id] === id,
+  [CollectionName.Materials]: (e: MaterialType, id: string) => e[MaterialKeyType.Id] === id,
+  [CollectionName.Rebars]: (e: RebarType, id: string) => e[RebarKeyType.Id] === id
+};
+
+const IdRerenceRenderer: React.FC<{ idType: IdKeysType; id: string }> = ({ idType, id }) => {
+  const collections = useCollectionStore((state) => state.collections);
+  const entry = collections[IdKeysCollectionMap[idType]].find((e) =>
+    findMethodMap[IdKeysCollectionMap[idType]](e as any, id)
+  );
+
+  return entry ? <ObjectRender name={id} value={entry} /> : <MissingData />;
+};
+
 export const EntryRenderer: React.FC<{ k: string; valueType: ValueType; value: any }> = ({ k, valueType, value }) => {
   if (valueType.endsWith('Array'))
     return <ArrayRenderer items={value} valueType={removeArrayFromEnd(valueType) as ValueType} />;
+
   switch (valueType) {
     case 'number':
       return <SimpleValue k={k} v={NumberRenderer(value as any)} />;
     case 'string':
+      if (IdKeys.includes(k as IdKeysType)) return <IdRerenceRenderer idType={k as IdKeysType} id={value} />;
       return <SimpleValue k={k} v={StringRenderer(value as any)} />;
     case 'UserCategory':
       return <SimpleValue k={k} v={UserCategoryRenderer(value as any)} />;
@@ -114,7 +174,7 @@ export const EntryRenderer: React.FC<{ k: string; valueType: ValueType; value: a
     case 'stringPair':
       return <SimpleValue k={k} v={StringPairRenderer(value as any)} />;
     default:
-      return <GenericUIRenderer item={value} />;
+      return <ObjectRender name={k} value={value} />;
   }
 };
 
