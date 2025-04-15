eace0882a22317a07ba1ad35e370b65120d44fff
MarcusTheCode <Marcusbyllemos@gmail.com>
2025-04-15 10:15:17 +0200

    Add endpoints for ComponentController

--- backend/slab2reuse/src/main/java/circrete/slab2reuse/controller/ComponentController.java
+++ backend/slab2reuse/src/main/java/circrete/slab2reuse/controller/ComponentController.java
@@ -4,9 +4,11 @@ import circrete.slab2reuse.model.Component;
 import circrete.slab2reuse.model.dto.ComponentDto;
 import circrete.slab2reuse.repository.ComponentRepository;
 import circrete.slab2reuse.service.ComponentService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/component")
@@ -21,6 +23,33 @@ public class ComponentController {
 
     private final ComponentRepository componentRepository;
     private final ComponentService componentService;
+
+    @GetMapping("/all")
+    public ResponseEntity<Iterable<ComponentDto>> getAllComponents() {
+        List<Component> components = componentRepository.findAll();
+        Iterable<ComponentDto> componentDtos = components.stream()
+                .map(componentService::componentToDto)
+                .toList();
+        return ResponseEntity.ok(componentDtos);
+    }
+
+    @PostMapping("/get-by-ids")
+    public ResponseEntity<List<ComponentDto>> getComponentById(@RequestBody List<Long> ids) {
+        if (ids == null) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        List<ComponentDto> componentDtos = ids.stream()
+                .map(componentService::getComponentById)
+                .map(componentService::componentToDto)
+                .toList();
+
+        if (componentDtos.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(componentDtos);
+    }
 
     @GetMapping("/{id}")
     public ResponseEntity<ComponentDto> getComponentById(@PathVariable Long id) {
