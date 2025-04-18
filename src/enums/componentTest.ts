d81b8ad15a33df46cb82655f7eeed99da3b4ebd8
JonasWard <43600893+JonasWard@users.noreply.github.com>
2025-04-18 11:09:30 +0200

    feat: creating data logic for displaying test data for geometryIds

---
+++ src/enums/componentTest.ts
@@ -0,0 +1,8 @@
+export enum ComponentTest {
+  ChemcicalTest = 'Chemical',
+  Core = 'Core',
+  Destructive = 'Destructive',
+  GPR = 'GPR',
+  Rebound = 'Rebound',
+  PreStressStrand = 'PreStressStrand'
+}
