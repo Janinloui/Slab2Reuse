5eed9786a66d6465765920e698410b956750c77c
Jonas Ward <jonas.vandenbulcke@gmail.com>
2025-04-24 18:23:12 +0200

    feat: deleted everything that has becoming obsolete / irrelevant because of slabType being replaced

--- src/views/GlobalViewer.tsx
+++
@@ -1,4 +0,0 @@
-import { NamedViews } from '../enums/viewer';
-import { ViewState } from '../enums/viewState';
-
-export const GlobalViewer: React.FC<{ viewer: NamedViews; viewState: ViewState }> = ({ viewer, viewState }) => {};
