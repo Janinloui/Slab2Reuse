b0b6c5960a6fd6cebc02b762012d85c351266893
Jonas Ward <jonas.vandenbulcke@gmail.com>
2025-04-28 18:06:38 +0200

    chore: tweak Header styling

--- frontend/src/globalUI/Header.tsx
+++ frontend/src/globalUI/Header.tsx
@@ -1,8 +1,14 @@
-import { Select } from 'antd';
+import { Button, Select } from 'antd';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { Slab2ReuseRoutes } from '../enums/routes';
 import { useTableStore } from '../state/tableStore';
 import { NamedViews } from '../enums/viewer';
+
+const options = [
+  { value: Slab2ReuseRoutes.Viewer, label: 'Both' },
+  { value: Slab2ReuseRoutes.TableOnly, label: 'Table' },
+  { value: Slab2ReuseRoutes.ThreeOnly, label: '3D' }
+];
 
 /**
  * Header Component
@@ -20,51 +26,32 @@ export const Header: React.FC = () => {
       style={{
         display: 'flex',
         flexDirection: 'row',
-        justifyItems: 'center',
+        alignItems: 'center',
         justifyContent: 'space-between',
-        padding: 10,
+        padding: 6,
         backgroundColor: '#9fcdf5',
         color: 'white'
       }}
     >
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'row',
-          gap: 8
-        }}
-      >
-        <div style={{ display: 'flex', justifyItems: 'center' }}>Slab2Reuse 💫</div>
-        <nav style={{ display: 'flex', flexDirection: 'row', gap: 4, alignItems: 'center' }}>
-          <a href='/' style={{ color: '#d15232' }}>
-            Home
-          </a>
-          <a href={import.meta.env.BASE_URL + `/#` + Slab2ReuseRoutes.TableOnly} style={{ color: '#d15232' }}>
-            Table
-          </a>
-          <a href={import.meta.env.BASE_URL + `/#` + Slab2ReuseRoutes.ThreeOnly} style={{ color: '#d15232' }}>
-            Three
-          </a>
-        </nav>
-      </div>
+      <nav style={{ display: 'flex', flexDirection: 'row', gap: 6, alignItems: 'center' }}>
+        <Button variant='filled' color='geekblue' onClick={() => navigate(Slab2ReuseRoutes.Landing)}>
+          Slab2Reuse 💫
+        </Button>
+        {options.map(({ value, label }) => (
+          <Button variant='solid' key={value} color='geekblue' onClick={() => navigate(value)}>
+            {label}
+          </Button>
+        ))}
+      </nav>
       <div style={{ gap: 8, display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
         <Select
           style={{ width: 200 }}
           value={viewer}
           variant='filled'
           onChange={(e) => useTableStore.getState().setViewer(e)}
-        >
-          {Object.values(NamedViews).map((e) => (
-            <Select.Option key={e} value={e}>
-              {e}
-            </Select.Option>
-          ))}
-        </Select>
-        <Select style={{ width: 80 }} variant='filled' value={location.pathname} onChange={(v) => navigate(v)}>
-          <Select.Option value={Slab2ReuseRoutes.Viewer}>viewer</Select.Option>
-          <Select.Option value={Slab2ReuseRoutes.TableOnly}>table</Select.Option>
-          <Select.Option value={Slab2ReuseRoutes.ThreeOnly}>3d</Select.Option>
-        </Select>
+          options={Object.values(NamedViews).map((e) => ({ value: e, label: e }))} // Use NamedViews enum for options
+        />
+        <Select variant='filled' value={location.pathname} onChange={(v) => navigate(v)} options={options} />
       </div>
     </header>
   );
