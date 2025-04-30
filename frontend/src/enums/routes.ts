261f7cc503ec0b6d8b32ee6858f0fb0c1374e060
Jonas Ward <jonas.vandenbulcke@gmail.com>
2025-04-30 17:09:11 +0200

    feat: route names

--- frontend/src/enums/routes.ts
+++ frontend/src/enums/routes.ts
@@ -11,3 +11,17 @@ export enum Slab2ReuseRoutes {
   ArchiveView = '/archive-view/:viewer',
   MaterialPassport = '/material-passport/:componentId'
 }
+
+export const Slab2ReuseRouteLocal: Record<Slab2ReuseRoutes, string> = {
+  [Slab2ReuseRoutes.Landing]: 'Home',
+  [Slab2ReuseRoutes.ExampleData]: 'Example Data',
+  [Slab2ReuseRoutes.RawTableView]: 'Objects View',
+  [Slab2ReuseRoutes.Viewer]: 'Both',
+  [Slab2ReuseRoutes.TableOnly]: 'Table Only',
+  [Slab2ReuseRoutes.ThreeOnly]: '3D Only',
+  [Slab2ReuseRoutes.BoilerPlate]: 'Boiler Plate Data',
+  [Slab2ReuseRoutes.LabView]: 'Lab View',
+  [Slab2ReuseRoutes.OnSiteView]: 'On Site View',
+  [Slab2ReuseRoutes.ArchiveView]: 'Archive View',
+  [Slab2ReuseRoutes.MaterialPassport]: 'Material Passport'
+};
