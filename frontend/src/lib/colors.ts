1ce938338f8bde1201b15da4cc505a058c646121
Jonas Ward <jonas.vandenbulcke@gmail.com>
2025-04-30 17:10:03 +0200

    feat: hexColor string to [0...1] rgb

--- frontend/src/lib/colors.ts
+++ frontend/src/lib/colors.ts
@@ -10,3 +10,10 @@ export const getColorForCondition = (condition: VisualCondition): string => {
       return '#FF6666';
   }
 };
+
+export const hexToRgb = (hex: string): [number, number, number] | undefined => {
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result
+    ? [parseInt(result[1], 16) / 255, parseInt(result[2], 16) / 255, parseInt(result[3], 16) / 255]
+    : undefined;
+};
